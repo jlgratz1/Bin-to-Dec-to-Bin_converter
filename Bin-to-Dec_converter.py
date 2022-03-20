da2063d60bb333d184f1e7a6235cd5ca2d8942f4
jlgratz1 <96318464+jlgratz1@users.noreply.github.com>
2022-03-20 11:26:35 +0300

    Add several improvements
    - no need to press "y" after each number to keep working - the program works until you enter 'q'
    - check for non-binary input works correctly now

--- Bin-to-Dec_converter.py
+++ Bin-to-Dec_converter.py
@@ -1,9 +1,12 @@
 dec_value = 0
 values_list = []
+keep_on = True
 
 def convert():
     
     global dec_value
+    global keep_on
+    global values_list
     bin_value = 0
     bin_list = []
     bin_check = True
@@ -16,38 +19,44 @@ def convert():
         if bin_value.isdigit():
             bin_list = list(map(int, str(bin_value)))
             for item in bin_list:
-                if 0 > item > 1:
+                if item > 1:
                     bin_check = False
             if bin_check:
                 wrong_input = False
             else:
                 print("It is not a binary value")
+                bin_check = True
+        elif bin_value == "q": # this is used to exit the program
+            keep_on = False
+            break
         else:
             print("It is not an integer")
-        
-    bin_list = bin_list[::-1]
+                
+    if wrong_input == False:
+        bin_list = bin_list[::-1]
 
-    for num in bin_list:
-        dec_value += num*2**power_ticker
-        power_ticker += 1
+        for num in bin_list:
+            dec_value += num*2**power_ticker
+            power_ticker += 1
+        
+        values_list.append(dec_value)
+        print(dec_value)
+
     
-    return dec_value
+    return dec_value, keep_on
 
 def run():
     
     global dec_value
     global values_list
-    keep_on = True
-    exit_value = 0
+    global keep_on
+
+    print("To quit press 'q'")
 
     while keep_on:
         convert()
-        values_list.append(dec_value)
-        print(dec_value)
-        exit_value = input("Need to convert anything else? [y/n]")
-        if exit_value == "n":
-            print(values_list)
-            keep_on = False 
         dec_value = 0     
+
+    print(values_list)
 
 run()
